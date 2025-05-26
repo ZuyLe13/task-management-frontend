3985071731200ca2ecdc5974994ebf476413eb07
dile <duyle131103@gmail.com>
2025-05-26 22:18:44 +0700

    feat: add sign out service

--- src/app/shared/_services/auth.service.ts
+++ src/app/shared/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
 
 interface AuthPayload {
   email: string;
@@ -11,7 +12,7 @@ interface AuthPayload {
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   signUp(payload: AuthPayload) {
     return this.http.post(`${environment.apiUrl}/auth/sign-up`, payload);
@@ -20,6 +21,15 @@ export class AuthService {
   signIn(payload: AuthPayload) {
     return this.http.post<{ accessToken: string }>(`${environment.apiUrl}/auth/sign-in`, payload, {
       withCredentials: true
+    });
+  }
+
+  signOut() {
+    return this.http.post(`${environment.apiUrl}/auth/sign-out`, {}, {
+      withCredentials: true
+    }).subscribe(() => {
+      localStorage.removeItem('accessToken');
+      this.router.navigate(['/sign-in']);
     });
   }
 
